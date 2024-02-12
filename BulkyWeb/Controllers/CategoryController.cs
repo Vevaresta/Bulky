e70f8654107b0c465343e6dae3b69f919157015e
Nikola Krpan <73753308+Vevaresta@users.noreply.github.com.>
2024-02-12 11:26:57 +0100

    Added Edit/Update Method with validation

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -16,11 +16,12 @@ namespace BulkyWeb.Controllers
 			List<Category> objCategoryList = _db.Categories.ToList();
 			return View(objCategoryList);
 		}
-
+		// If I don't define anything it is by default GET Method
 		public IActionResult Create()
 		{
 			return View();
 		}
+
 		[HttpPost]
         public IActionResult Create(Category obj)
         {
@@ -37,5 +38,35 @@ namespace BulkyWeb.Controllers
 			}
 			return View();
         }
+
+        public IActionResult Edit(int? id)
+        {
+			if(id==null || id == 0)
+			{
+				return NotFound();
+			}
+			Category? obj = _db.Categories.Find(id);
+            //Category? obj = _db.Categories.FirstOrDefault(u=>u.Id==id);
+            //Category? obj = _db.Categories.Where(u=>u.Id==id)FirstOrDefault();
+
+            if (obj == null) 
+			{ 
+				return NotFound(); 
+			}
+            
+			return View(obj);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
     }
 }
