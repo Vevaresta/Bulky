5f8791a6c7c9f82c589bdec80e645cbd0d35fc3a
Nikola Krpan <73753308+Vevaresta@users.noreply.github.com.>
2024-02-13 10:00:28 +0100

    Added TempData for views

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -34,6 +34,7 @@ namespace BulkyWeb.Controllers
 			{
 				_db.Categories.Add(obj);
 				_db.SaveChanges();
+                TempData["success"] = "Category created successfully";
 				return RedirectToAction("Index");
 			}
 			return View();
@@ -63,6 +64,7 @@ namespace BulkyWeb.Controllers
             {
                 _db.Categories.Update(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
                 return RedirectToAction("Index");
             }
             return View();
@@ -95,6 +97,7 @@ namespace BulkyWeb.Controllers
             }
             _db.Categories.Remove(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
             return RedirectToAction("Index");     
         }
 
