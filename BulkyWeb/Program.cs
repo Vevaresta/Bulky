cb02b4fd9570ba804f303bfbb791cdadd4d9508f
Nikola Krpan <73753308+Vevaresta@users.noreply.github.com.>
2024-02-21 13:00:31 +0100

    Registered ICategoryRepo and CategoryRepo with dependency injection container

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -1,12 +1,16 @@
+using Bulky.DataAccess.Repository;
+using Bulky.DataAccess.Repository.IRepository;
 using Bulky.DataAcess.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the dependency injection container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options=> 
 	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 
 var app = builder.Build();
 
