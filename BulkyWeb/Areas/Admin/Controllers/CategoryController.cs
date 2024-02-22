d022a23a7365bd44ff86fbe8133c5e5d4fe410e9
Nikola Krpan <73753308+Vevaresta@users.noreply.github.com.>
2024-02-22 12:36:34 +0100

    Added Areas and rearranged files

--- BulkyWeb/Areas/Admin/Controllers/CategoryController.cs
+++ BulkyWeb/Areas/Admin/Controllers/CategoryController.cs
@@ -3,60 +3,61 @@ using Bulky.DataAcess.Data;
 using Bulky.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace BulkyWeb.Controllers
+namespace BulkyWeb.Areas.Admin.Controllers
 {
-	public class CategoryController : Controller
-	{
-		private readonly IUnitOfWork _unitOfWork;
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
         public CategoryController(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
         }
         public IActionResult Index()
-		{
-			List<Category> objCategoryList = _unitOfWork.Category.GetAll().ToList();
-			return View(objCategoryList);
-		}
-		// If I don't define anything it is by default GET Method
-		public IActionResult Create()
-		{
-			return View();
-		}
+        {
+            List<Category> objCategoryList = _unitOfWork.Category.GetAll().ToList();
+            return View(objCategoryList);
+        }
+        // If I don't define anything it is by default GET Method
+        public IActionResult Create()
+        {
+            return View();
+        }
 
-		[HttpPost]
+        [HttpPost]
         public IActionResult Create(Category obj)
         {
-			if (obj.Name == obj.DisplayOrder.ToString())
-			{
-				ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
-			}
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
 
-			if (ModelState.IsValid)
-			{
+            if (ModelState.IsValid)
+            {
                 _unitOfWork.Category.Add(obj);
                 _unitOfWork.Save();
                 TempData["success"] = "Category created successfully";
-				return RedirectToAction("Index");
-			}
-			return View();
+                return RedirectToAction("Index");
+            }
+            return View();
         }
 
         public IActionResult Edit(int? id)
         {
-			if(id==null || id == 0)
-			{
-				return NotFound();
-			}
-			Category? obj = _unitOfWork.Category.Get(u => u.Id == id);
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? obj = _unitOfWork.Category.Get(u => u.Id == id);
             //Category? obj = _db.Categories.FirstOrDefault(u=>u.Id==id);
             //Category? obj = _db.Categories.Where(u=>u.Id==id)FirstOrDefault();
 
-            if (obj == null) 
-			{ 
-				return NotFound(); 
-			}
-            
-			return View(obj);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
         }
         [HttpPost]
         public IActionResult Edit(Category obj)
@@ -65,7 +66,7 @@ namespace BulkyWeb.Controllers
             {
                 _unitOfWork.Category.Update(obj);
                 _unitOfWork.Save();
-                 TempData["success"] = "Category updated successfully";
+                TempData["success"] = "Category updated successfully";
                 return RedirectToAction("Index");
             }
             return View();
@@ -91,7 +92,7 @@ namespace BulkyWeb.Controllers
         public IActionResult DeletePOST(int? id)
         {
             Category? obj = _unitOfWork.Category.Get(u => u.Id == id);
-            
+
             if (obj == null)
             {
                 return NotFound();
@@ -99,7 +100,7 @@ namespace BulkyWeb.Controllers
             _unitOfWork.Category.Remove(obj);
             _unitOfWork.Save();
             TempData["success"] = "Category deleted successfully";
-            return RedirectToAction("Index");     
+            return RedirectToAction("Index");
         }
 
     }
