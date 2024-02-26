a3679649a993c50460e0be7d936b50cc4a3b3b8f
Nikola Krpan <73753308+Vevaresta@users.noreply.github.com.>
2024-02-26 17:01:25 +0100

    Added Product CRUD and Repository

--- Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -12,7 +12,7 @@ namespace Bulky.DataAcess.Data
         }
 
         public DbSet<Category> Categories { get; set; }
-        public DbSet<Category> Products { get; set; }
+        public DbSet<Product> Products { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
