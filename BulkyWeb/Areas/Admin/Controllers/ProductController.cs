f2e55dbdbc45cfcb37327fd62a000cacf3e508b5
Nikola Krpan <73753308+Vevaresta@users.noreply.github.com.>
2024-03-07 17:38:24 +0100

    Added Upsert instead Create and Edit in Product

--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -21,7 +21,7 @@ namespace BulkyWeb.Areas.Admin.Controllers
             return View(objProductList);
         }
         // If I don't define anything it is by default GET Method
-        public IActionResult Create()
+        public IActionResult Upsert(int? id)
         { 
             ProductVM productVM = new()
             {
@@ -32,52 +32,42 @@ namespace BulkyWeb.Areas.Admin.Controllers
                 }),
                 Product = new Product()
             };
-            return View(productVM);
+            if (id == null || id == 0)
+            {
+                // create
+                return View(productVM);
+            }
+            else
+            {
+                //update
+                productVM.Product = _unitOfWork.Product.Get(u => u.Id == id);
+                return View(productVM);
+            }
         }
 
         [HttpPost]
-        public IActionResult Create(ProductVM obj)
+        public IActionResult Upsert(ProductVM productVM, IFormFile? file)
         {
 
             if (ModelState.IsValid)
             {
-                _unitOfWork.Product.Add(obj.Product);
+                _unitOfWork.Product.Add(productVM.Product);
                 _unitOfWork.Save();
                 TempData["success"] = "Product created successfully";
                 return RedirectToAction("Index");
             }
-            return View();
+            else
+            {
+                productVM.CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                });
+                 return View(productVM);
+            }
+          
         }
 
-        public IActionResult Edit(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-            Product? productFromDb = _unitOfWork.Product.Get(u => u.Id == id);
-            //Category? obj = _db.Categories.FirstOrDefault(u=>u.Id==id);
-            //Category? obj = _db.Categories.Where(u=>u.Id==id)FirstOrDefault();
-
-            if (productFromDb == null)
-            {
-                return NotFound();
-            }
-
-            return View(productFromDb);
-        }
-        [HttpPost]
-        public IActionResult Edit(Product obj)
-        {
-            if (ModelState.IsValid)
-            {
-                _unitOfWork.Product.Update(obj);
-                _unitOfWork.Save();
-                TempData["success"] = "Product updated successfully";
-                return RedirectToAction("Index");
-            }
-            return View();
-        }
         public IActionResult Delete(int? id)
         {
             if (id == null || id == 0)
